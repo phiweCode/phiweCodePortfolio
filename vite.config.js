c9c411825324a2a434c96f99fb45447c0ee4dc6f
phiweCode <thabo.s.mngoma@gmail.com>
2024-05-12 13:22:52 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/phiweCodePortfolio/",
+  server: { 
+    watch: {
+      usePolling: true, 
+    }, 
+    port: 3000
+  }
 })
